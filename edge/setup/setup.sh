654a9831f1987a16ce4ddb8868305111971df006
ALESSANDRO RAPITI <40713438+KeyserDSoze@users.noreply.github.com>
2021-01-21 11:24:26 +0100

    Update setup.sh

--- edge/setup/setup.sh
+++ edge/setup/setup.sh
@@ -17,6 +17,9 @@ RED='\033[0;31m'
 BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
+echo -e "\n${RED}Please insert the prefix"
+read -p ">> " MY_BEST_NAME
+
 # script configuration
 BASE_URL='https://raw.githubusercontent.com/KeyserDSoze/live-video-analytics/master/edge/setup' # location of remote files used by the script
 DEFAULT_REGION='westeurope'
@@ -30,11 +33,11 @@ DEPLOYMENT_MANIFEST_URL="$BASE_URL/deployment.template.json"
 DEPLOYMENT_MANIFEST_FILE='edge-deployment/deployment.amd64.json'
 ROLE_DEFINITION_URL="$BASE_URL/LVAEdgeUserRoleDefinition.json"
 ROLE_DEFINITION_FILE='role_definition.json'
-RESOURCE_GROUP='lva-sample-resources'
-IOT_EDGE_VM_NAME='lva-sample-iot-edge-device'
+RESOURCE_GROUP='$MY_BEST_NAME-lva-rg'
+IOT_EDGE_VM_NAME='$MY_BEST_NAME-lva-iot-edge-device'
 IOT_EDGE_VM_ADMIN='lvaadmin'
 IOT_EDGE_VM_PWD="lvaPwdKS@$(shuf -i 1000-999999 -n 1)"
-CLOUD_SHELL_FOLDER="$HOME/clouddrive/lva-sample"
+CLOUD_SHELL_FOLDER="$HOME/clouddrive/lva-$MY_BEST_NAME"
 APPDATA_FOLDER_ON_DEVICE="/var/lib/azuremediaservices"
 
 checkForError() {
